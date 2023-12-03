b62156427458501916aa7a262e66a5a405cf6ac0
Vince Melia <vince@drifta.co.uk>
2023-12-03 12:19:54 +0000

    Empty project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flame_demo
+
+https://www.youtube.com/watch?v=Kwn1eHZP3C4
---
+++ android/app/src/main/kotlin/com/example/flame_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flame_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
